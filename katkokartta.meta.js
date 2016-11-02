8c50c5db0680925a5bfcf35b6a21125a6f4c5ea4
Ville <vtuomi@a88-113-71-59.elisa-laajakaista.fi>
2016-11-02 20:59:43 +0200

    adds .meta.js and download urls

---
+++ katkokartta.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Kätkösivun kartta
+// @namespace   fi.geokatkot
+// @description Lisää MML-kartan Geocaching.com-kätkösivulle.
+// @include     *.geocaching.com/geocache/*
+// @require 	  https://www.geocaching.com/static/js/leaflet/0.7.2/leaflet.js
+// @downloadURL https://github.com/geokatkot/katkokartta/raw/master/katkokartta.user.js
+// @updateURL   https://github.com/geokatkot/katkokartta/raw/master/katkokartta.meta.js
+// @version     1.0
+// @grant       none
+// ==/UserScript==
